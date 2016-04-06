5e31a6d591cec37b979126019f9dcbdf5900ff4f
maliang <maliang6111a@qq.com>
2016-04-06 14:35:28 +0800

    修改js代码

--- javascript/base64.js
+++ javascript/base64.js
@@ -1,215 +1,188 @@
-(function(){
-	var BASE64_MAPPING = [
-		'A','B','C','D','E','F','G','H',
-		'I','J','K','L','M','N','O','P',
-		'Q','R','S','T','U','V','W','X',
-		'Y','Z','a','b','c','d','e','f',
-		'g','h','i','j','k','l','m','n',
-		'o','p','q','r','s','t','u','v',
-		'w','x','y','z','0','1','2','3',
-		'4','5','6','7','8','9','+','/'
-	];
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var base64DecodeChars = new Array(
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
 
-	/**
-	 *ascii convert to binary
-	 */
-	var _toBinary = function(ascii){
-		var binary = new Array();
-		while(ascii > 0){
-			var b = ascii%2;
-			ascii = Math.floor(ascii/2);
-			binary.push(b);
-		}
-		/*
-		var len = binary.length;
-		if(6-len > 0){
-			for(var i = 6-len ; i > 0 ; --i){
-				binary.push(0);
-			}
-		}*/
-		binary.reverse();
-		return binary;
-	};
+function base64encode(str) {
+    var out, i, len;
+    var c1, c2, c3;
 
-	/**
-	 *binary convert to decimal
-	 */
-	var _toDecimal  = function(binary){
-		var dec = 0;
-		var p = 0;
-		for(var i = binary.length-1 ; i >= 0 ; --i){
-			var b = binary[i];
-			if(b == 1){
-				dec += Math.pow(2 , p);
-			}
-			++p;
-		}
-		return dec;
-	};
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+    c1 = str.charCodeAt(i++) & 0xff;
+    if(i == len)
+    {
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+        out += "==";
+        break;
+    }
+    c2 = str.charCodeAt(i++);
+    if(i == len)
+    {
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+        out += "=";
+        break;
+    }
+    c3 = str.charCodeAt(i++);
+    out += base64EncodeChars.charAt(c1 >> 2);
+    out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+    out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+    out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
+}
 
-	/**
-	 *unicode convert to utf-8
-	 */
-	var _toUTF8Binary = function(c , binaryArray){
-		var mustLen = (8-(c+1)) + ((c-1)*6);
-		var fatLen = binaryArray.length;
-		var diff = mustLen - fatLen;
-		while(--diff >= 0){
-			binaryArray.unshift(0);
-		}
-		var binary = [];
-		var _c = c;
-		while(--_c >= 0){
-			binary.push(1);
-		}
-		binary.push(0);
-		var i = 0 , len = 8 - (c+1);
-		for(; i < len ; ++i){
-			binary.push(binaryArray[i]);
-		}
+function base64decode(str) {
+    var c1, c2, c3, c4;
+    var i, len, out;
 
-		for(var j = 0 ; j < c-1 ; ++j){
-			binary.push(1);
-			binary.push(0);
-			var sum = 6;
-			while(--sum >= 0){
-				binary.push(binaryArray[i++]);
-			}
-		}
-		return binary;
-	};
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+    /* c1 */
+    do {
+        c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+    } while(i < len && c1 == -1);
+    if(c1 == -1)
+        break;
 
-	var __BASE64 = {
-			/**
-			 *BASE64 Encode
-			 */
-			encoder:function(str){
-				var base64_Index = [];
-				var binaryArray = [];
-				for(var i = 0 , len = str.length ; i < len ; ++i){
-					var unicode = str.charCodeAt(i);
-					var _tmpBinary = _toBinary(unicode);
-					if(unicode < 0x80){
-						var _tmpdiff = 8 - _tmpBinary.length;
-						while(--_tmpdiff >= 0){
-							_tmpBinary.unshift(0);
-						}
-						binaryArray = binaryArray.concat(_tmpBinary);
-					}else if(unicode >= 0x80 && unicode <= 0x7FF){
-						binaryArray = binaryArray.concat(_toUTF8Binary(2 , _tmpBinary));
-					}else if(unicode >= 0x800 && unicode <= 0xFFFF){//UTF-8 3byte
-						binaryArray = binaryArray.concat(_toUTF8Binary(3 , _tmpBinary));
-					}else if(unicode >= 0x10000 && unicode <= 0x1FFFFF){//UTF-8 4byte
-						binaryArray = binaryArray.concat(_toUTF8Binary(4 , _tmpBinary));	
-					}else if(unicode >= 0x200000 && unicode <= 0x3FFFFFF){//UTF-8 5byte
-						binaryArray = binaryArray.concat(_toUTF8Binary(5 , _tmpBinary));
-					}else if(unicode >= 4000000 && unicode <= 0x7FFFFFFF){//UTF-8 6byte
-						binaryArray = binaryArray.concat(_toUTF8Binary(6 , _tmpBinary));
-					}
-				}
+    /* c2 */
+    do {
+        c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+    } while(i < len && c2 == -1);
+    if(c2 == -1)
+        break;
 
-				var extra_Zero_Count = 0;
-				for(var i = 0 , len = binaryArray.length ; i < len ; i+=6){
-					var diff = (i+6)-len;
-					if(diff == 2){
-						extra_Zero_Count = 2;
-					}else if(diff == 4){
-						extra_Zero_Count = 4;
-					}
-					//if(extra_Zero_Count > 0){
-					//	len += extra_Zero_Count+1;
-					//}
-					var _tmpExtra_Zero_Count = extra_Zero_Count;
-					while(--_tmpExtra_Zero_Count >= 0){
-						binaryArray.push(0);
-					}
-					base64_Index.push(_toDecimal(binaryArray.slice(i , i+6)));
-				}
+    out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
 
-				var base64 = '';
-				for(var i = 0 , len = base64_Index.length ; i < len ; ++i){
-					base64 += BASE64_MAPPING[base64_Index[i]];
-				}
+    /* c3 */
+    do {
+        c3 = str.charCodeAt(i++) & 0xff;
+        if(c3 == 61)
+        return out;
+        c3 = base64DecodeChars[c3];
+    } while(i < len && c3 == -1);
+    if(c3 == -1)
+        break;
 
-				for(var i = 0 , len = extra_Zero_Count/2 ; i < len ; ++i){
-					base64 += '=';
-				}
-				return base64;
-			},
-			/**
-			 *BASE64  Decode for UTF-8 
-			 */
-			decoder : function(_base64Str){
-				var _len = _base64Str.length;
-				var extra_Zero_Count = 0;
-				/**
-				 *计算在进行BASE64编码的时候，补了几个0
-				 */
-				if(_base64Str.charAt(_len-1) == '='){
-					//alert(_base64Str.charAt(_len-1));
-					//alert(_base64Str.charAt(_len-2));
-					if(_base64Str.charAt(_len-2) == '='){//两个等号说明补了4个0
-						extra_Zero_Count = 4;
-						_base64Str = _base64Str.substring(0 , _len-2);
-					}else{//一个等号说明补了2个0
-						extra_Zero_Count = 2;
-						_base64Str = _base64Str.substring(0 , _len - 1);
-					}
-				}
+    out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
 
-				var binaryArray = [];
-				for(var i = 0 , len = _base64Str.length; i < len ; ++i){
-					var c = _base64Str.charAt(i);
-					for(var j = 0 , size = BASE64_MAPPING.length ; j < size ; ++j){
-						if(c == BASE64_MAPPING[j]){
-							var _tmp = _toBinary(j);
-							/*不足6位的补0*/
-							var _tmpLen = _tmp.length;
-							if(6-_tmpLen > 0){
-								for(var k = 6-_tmpLen ; k > 0 ; --k){
-									_tmp.unshift(0);
-								}
-							}
-							binaryArray = binaryArray.concat(_tmp);
-							break;
-						}
-					}
-				}
+    /* c4 */
+    do {
+        c4 = str.charCodeAt(i++) & 0xff;
+        if(c4 == 61)
+        return out;
+        c4 = base64DecodeChars[c4];
+    } while(i < len && c4 == -1);
+    if(c4 == -1)
+        break;
+    out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+}
 
-				if(extra_Zero_Count > 0){
-					binaryArray = binaryArray.slice(0 , binaryArray.length - extra_Zero_Count);
-				}
+function utf16to8(str) {
+    var out, i, len, c;
 
-				var unicode = [];
-				var unicodeBinary = [];
-				for(var i = 0 , len = binaryArray.length ; i < len ; ){
-					if(binaryArray[i] == 0){
-						unicode=unicode.concat(_toDecimal(binaryArray.slice(i,i+8)));
-						i += 8;
-					}else{
-						var sum = 0;
-						while(i < len){
-							if(binaryArray[i] == 1){
-								++sum;
-							}else{
-								break;
-							}
-							++i;
-						}
-						unicodeBinary = unicodeBinary.concat(binaryArray.slice(i+1 , i+8-sum));
-						i += 8 - sum;
-						while(sum > 1){
-							unicodeBinary = unicodeBinary.concat(binaryArray.slice(i+2 , i+8));
-							i += 8;
-							--sum;
-						}
-						unicode = unicode.concat(_toDecimal(unicodeBinary));
-						unicodeBinary = [];
-					}
-				}
-				return unicode;
-			}
-	};
+    out = "";
+    len = str.length;
+    for(i = 0; i < len; i++) {
+    c = str.charCodeAt(i);
+    if ((c >= 0x0001) && (c <= 0x007F)) {
+        out += str.charAt(i);
+    } else if (c > 0x07FF) {
+        out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+        out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+        out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+    } else {
+        out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+        out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+    }
+    }
+    return out;
+}
 
-	window.BASE64 = __BASE64;
-})();
+function utf8to16(str) {
+    var out, i, len, c;
+    var char2, char3;
+
+    out = "";
+    len = str.length;
+    i = 0;
+    while(i < len) {
+    c = str.charCodeAt(i++);
+    switch(c >> 4)
+    { 
+      case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+        // 0xxxxxxx
+        out += str.charAt(i-1);
+        break;
+      case 12: case 13:
+        // 110x xxxx   10xx xxxx
+        char2 = str.charCodeAt(i++);
+        out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+        break;
+      case 14:
+        // 1110 xxxx  10xx xxxx  10xx xxxx
+        char2 = str.charCodeAt(i++);
+        char3 = str.charCodeAt(i++);
+        out += String.fromCharCode(((c & 0x0F) << 12) |
+                       ((char2 & 0x3F) << 6) |
+                       ((char3 & 0x3F) << 0));
+        break;
+    }
+    }
+
+    return out;
+}
+
+function CharToHex(str) {
+    var out, i, len, c, h;
+    out = "";
+    len = str.length;
+    i = 0;
+    while(i < len) 
+    {
+	    c = str.charCodeAt(i++);
+	    h = c.toString(16);
+	    if(h.length < 2)
+	    	h = "0" + h;
+	    
+	    out += "\\x" + h + " ";
+	    if(i > 0 && i % 8 == 0)
+	    	out += "\r\n";
+    }
+
+    return out;
+}
+
+function doEncode() {
+	var src = document.getElementById('src').value;
+	document.getElementById('dest').value = base64encode(utf16to8(src));
+}
+
+function doDecode() {
+	var src = document.getElementById('src').value;
+	var opts = document.getElementById('opt');
+
+	if(opts.checked)
+	{
+		document.getElementById('dest').value = CharToHex(base64decode(src));
+	}
+	else
+	{
+		document.getElementById('dest').value = utf8to16(base64decode(src));
+	}
+}
